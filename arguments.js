e0892a3dc2d9e16ebe55227d1391910f8804410a
Sanjeevan Thorat <sanjeevan.k.thorat@gmail.com>
2023-07-04 17:39:43 +0530

    modified secrets

--- arguments.js
+++ arguments.js
@@ -1,7 +1,7 @@
 module.exports=[
     150,
-    ["0x7F6Ff3AFEd10e5F73999c440cEa5b05552B2aED2"],//proposers array
-    ["0x7F6Ff3AFEd10e5F73999c440cEa5b05552B2aED2"],//excutors array
-    "0x0ce02e7198759402b7764cC8E83A6e09DdEBc6fb"//metamask goerli tesntnet acc address
+    [""],//proposers array
+    [""],//excutors array
+    ""//metamask goerli tesntnet acc address
     
 ]
